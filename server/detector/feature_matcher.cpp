ff5950e2929cf6f40cf801d9dddf4b4e62495778
Stefan Ghiaus <sghiaus@gmail.com>
2014-11-13 19:18:52 +0200

    Cleans up feature detection code.

--- server/detector/feature_matcher.cpp
+++ server/detector/feature_matcher.cpp
@@ -11,19 +11,18 @@
 using namespace cv;
 using namespace std;
 
+namespace {
+int SURF_MIN_HESSIAN = 400;
+}  // namespace
+
 namespace detector {
 
-// TODO(sghiaus): Also scale down the images so the computation is faster.
-// TODO(sghiaus): Template features can be precomputed and input image features
-// should only be computed once.
 int ComputeFeatureMatchScore(const Mat& input_image,
                              const string& template_path) {
     Mat template_image = imread(template_path, CV_LOAD_IMAGE_GRAYSCALE);
 
-    // Using SURF.
     // Detect keypoints.
-    int minHessian = 400;
-    SurfFeatureDetector detector(minHessian);
+    SurfFeatureDetector detector(SURF_MIN_HESSIAN);
     vector<KeyPoint> input_kps, template_kps;
     detector.detect(input_image, input_kps);
     detector.detect(template_image, template_kps);
@@ -37,22 +36,22 @@ int ComputeFeatureMatchScore(const Mat& input_image,
     // Perform matching.
     FlannBasedMatcher matcher;
     vector<DMatch> matches;
-    matcher.match(input_des, template_des, matches);
+    matcher.match(template_des, input_des, matches);
 
-    double max_dist = 0; double min_dist = 100;
+    // Keep only "good matches".
+    vector<DMatch> good_matches;
 
-    for (int i = 0; i < input_des.rows; i++) {
+    double max_dist = 0;
+    double min_dist = 100;
+
+    for (int i = 0; i < template_des.rows; ++i) {
         double dist = matches[i].distance;
         if (dist < min_dist) min_dist = dist;
         if (dist > max_dist) max_dist = dist;
     }
 
-
-    // Keep only "good matches".
-    vector<DMatch> good_matches;
-
-    for (int i = 0; i < input_des.rows; i++) {
-        if (matches[i].distance <= max(2 * min_dist, 0.02)) {
+    for (int i = 0; i < template_des.rows; ++i) {
+        if (matches[i].distance < 3 * min_dist) {
             good_matches.push_back(matches[i]);
         }
     }

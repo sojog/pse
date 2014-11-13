f43a4b49bafcec63de776aa7b4226cc847d202ae
Stefan Ghiaus <sghiaus@gmail.com>
2014-11-13 14:34:33 +0200

    Updates the freebase fetcher to use batching and skip entries with incomplete metadata.

--- server/freebase/fetch.py
+++ server/freebase/fetch.py
@@ -1,3 +1,4 @@
+import imghdr
 import json
 import os
 import sys
@@ -20,39 +21,49 @@ CONTAINER_FOLDER = 'data/'
 params = {
         'filter': '(all type:/visual_art/artwork /visual_art/artwork/art_form:"Painting")',
         'key': api_key,
-        'limit': '100',
+        'limit': str(BATCH_SIZE),
         'output' : '(/common/topic/image /common/topic/description)',
-        'lang' : 'en'
+        'lang' : 'en',
 }
 
 # Perform query.
 remaining_requests = num_requests
 print("Starting download.")
 print("Remaining: " + str(remaining_requests))
+cursor = 0
+
 while remaining_requests > 0:
+    print('Starting new batch request.')
     if remaining_requests > BATCH_SIZE:
         current_size = BATCH_SIZE
     else:
         current_size = remaining_requests
 
-    params['limit'] = str(current_size)
+    params['cursor'] = str(cursor)
+
     url = SERVICE_URL + '?' + urllib.urlencode(params)
     batch_response = json.loads(urllib.urlopen(url).read())
 
+    print(len(batch_response['result']))
     for result in batch_response['result']:
         mid = result['mid']
+        try:
+            image_mid = result['output']['/common/topic/image']['/common/topic/image'][0]['mid']            
+            name = result['name']
+            description = result['output']['/common/topic/description']['/common/topic/description'][0]
+        except:
+            print('Mid', mid, 'has incomplete metadata. Skipping.')
+            continue
 
         # Create container folder.
         mid_folder = CONTAINER_FOLDER + mid[3:] + '/'  # Removing the /m/ prefix.
         if not os.path.exists(mid_folder):
             os.makedirs(mid_folder)
 
-        # Read metadata.
-        metadata = dict()
-        metadata['name'] = result['name']
-        metadata['description'] = result['output']['/common/topic/description']['/common/topic/description'][0]
-
         # Write metadata.
+        metadata = dict()
+        metadata['name'] = name
+        metadata['description'] = description
         with open(mid_folder + 'metadata.json', 'w') as outfile:
             json.dump(metadata, outfile)
 
@@ -63,9 +74,17 @@ while remaining_requests > 0:
                 'maxheight' : '512'
         }
         image_url = IMAGE_BASE_URL + image_mid + '?' + urllib.urlencode(image_params)
-        image_save_path = mid_folder + 'image.png'
+        image_save_path = mid_folder + 'image'
         urllib.urlretrieve(image_url, image_save_path)
 
+        # Add image extension.
+        extension = imghdr.what(image_save_path)
+        os.rename(image_save_path, image_save_path + '.' + extension)
+
         remaining_requests -= 1
+        cursor += 1
         if remaining_requests != 0:
             print("Remaining: " + str(remaining_requests))
+        else:
+            break
+
